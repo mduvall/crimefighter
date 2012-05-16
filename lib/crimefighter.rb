2062cdd6ea0dc4ee380580ee83b156950b556af1
Matthew DuVall <mduvall89@gmail.com>
2012-05-16 00:52:51 -0700

    Refactors for readability

--- lib/crimefighter.rb
+++ lib/crimefighter.rb
@@ -15,24 +15,23 @@ class CrimeFighter
   CRIMES = {}
 
   def self.get_sf_crime_doc
-    today_date = Time.now.to_date.strftime("%m/%d/%Y")
-    last_week_date = (Time.now.to_date - 7).strftime("%m/%d/%Y")
+    today_date      = Time.now.to_date.strftime("%m/%d/%Y")
+    last_week_date  = (Time.now.to_date - 7).strftime("%m/%d/%Y")
     # Who love's ASP.NET generated URLs again...?
-    todays_url = URL[0] + last_week_date + URL[1] + today_date + URL[2]
-    html = Nokogiri::HTML(open(todays_url))
+    todays_url      = URL[0] + last_week_date + URL[1] + today_date + URL[2]
+    html            = Nokogiri::HTML(open(todays_url))
   end    
 
-  def self.grab_sf_crime
+  def self.populate_sf_crime
     html = self.get_sf_crime_doc
-
     html.css("div.report table.report-grid tr").each do |table_row|
       logo, crime, case_number, address, agency, date = table_row.css("td")
       if crime && address && case_number && date
         crime_name = crime.content.split("Service:")[1].strip
         CRIMES[case_number.content] = {
-          :crime => crime_name,
-          :address => address.content,
-          :date => date.content
+          :crime    => crime_name,
+          :address  => address.content,
+          :date     => date.content
         }
       end
     end  
@@ -40,17 +39,13 @@ class CrimeFighter
   end
 
   def self.sf_crime_json
-    if CRIMES.keys.length > 0
-      CRIMES.to_json
-    else
-      self.grab_sf_crime.to_json
-    end
+    self.populate_sf_crime if CRIMES.keys.length == 0
+    CRIMES.to_json
   end
 
   def self.sf_crime_to_csv
+    self.populate_sf_crime if CRIMES.keys.length == 0
     CSV.open('crimes.csv', 'wb') do |csv|
-      self.grab_sf_crime if CRIMES.keys.length == 0
-
       CRIMES.each_key do |case_number|
         crime = CRIMES[case_number]
         csv << [crime[:crime], crime[:address], crime[:date]]
