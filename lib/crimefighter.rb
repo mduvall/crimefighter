567f14c725eabb5be17d37224a10234ab276e215
Matthew DuVall <mduvall89@gmail.com>
2012-05-16 00:40:10 -0700

    Put into JSON document in class constant

--- lib/crimefighter.rb
+++ lib/crimefighter.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'date'
+require 'json'
 
 class CrimeFighter
   URL = [
@@ -9,6 +10,8 @@ class CrimeFighter
     "+00:00:00&de=",
     "+23:59:00&ccs=AR,AS,BU,DP,DR,DU,FR,HO,VT,RO,SX,TH,VA,VB,WE&xmin=-13635654.02571286&ymin=4543995.01718537&xmax=-13619640.468287138&ymax=4551438.072814629"
   ]
+
+  CRIMES = {}
 
   def self.get_sf_crime_doc
     today_date = Time.now.to_date.strftime("%m/%d/%Y")
@@ -21,12 +24,11 @@ class CrimeFighter
   def self.grab_sf_crime
     html = self.get_sf_crime_doc
 
-    crimes = {}
     html.css("div.report table.report-grid tr").each do |table_row|
       logo, crime, case_number, address, agency, date = table_row.css("td")
       if crime && address && case_number && date
         crime_name = crime.content.split("Service:")[1].strip
-        crimes[case_number.content] = {
+        CRIMES[case_number.content] = {
           :crime => crime_name,
           :address => address.content,
           :date => date.content
@@ -36,9 +38,14 @@ class CrimeFighter
         puts "\tDate: " + date.content
       end
     end  
-    crimes
+    CRIMES
   end
 
+  def self.sf_crime_json
+    if CRIMES.keys.length > 0
+      CRIMES.to_json
+    else
+      self.grab_sf_crime.to_json
+    end
+  end
 end
-
-CrimeFighter.grab_sf_crime
