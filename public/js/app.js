e8f969bbc14982463cc4d6bbc54720d6815894fb
Matthew DuVall <mduvall89@gmail.com>
2012-05-26 10:13:23 -0700

    Some dimensions on crimes and dates

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-  CrimeFighter.getJSONFromServer();
-  window.setTimeout(CrimeFighter.setupDataContainers, 500);
+  CrimeFighter.buildBarChart();
 });
 
 CrimeFighter = {};
@@ -11,11 +10,13 @@ CrimeFighter.getJSONFromServer = function() {
   });
 };
 
-CrimeFighter.rawCrimeToArray = function() {
-  if (CrimeFighter.rawCrimes) {
+CrimeFighter.rawCrimeToArray = function(crimes) {
+  if (crimes || CrimeFighter.rawCrimes) {
+    var crimeRecords = crimes || CrimeFighter.rawCrimes;
     var ary = [];
-    for (var crime in CrimeFighter.rawCrimes) {
-      ary.push(CrimeFighter.rawCrimes[crime]);
+    for (var crime in crimeRecords) {
+      var crimeRecord = crimeRecords[crime];
+      ary.push(crimeRecord);
     }
     return ary;
   }
@@ -25,12 +26,31 @@ CrimeFighter.rawCrimeToArray = function() {
 CrimeFighter.setupDataContainers = function() {
   var crimeContainer = CrimeFighter.rawCrimeToArray();
   if (crimeContainer) {
-    CrimeFighter.crimeCrossfilter = crossfilter(crimeContainer);
+    CrimeFighter.CrimeCrossfilter = crossfilter(crimeContainer);
   }
 };
 
 CrimeFighter.dimensionByCrimeType = function() {
   if (CrimeFighter.crimeCrossfilter) {
-    return CrimeFighter.crimeCrossfilter.dimension(function(crime) { crime.crime; });
+    return CrimeFighter.CrimeCrossfilter.dimension(function(crimeRecord) { return crimeRecord.crime; });
   }
 };
+
+CrimeFighter.parseDateStrings = function(crimes) {
+  var crimeArray = CrimeFighter.rawCrimeToArray(crimes);
+  for (var i = 0; i < crimeArray.length; i++) {
+    record = crimeArray[i];
+    record.date = d3.time.format(record.date);
+  }
+  return crimeArray;
+}
+
+CrimeFighter.buildBarChart = function() {
+  d3.json("../crimes.json", function(crimes) { 
+    var crimeArray = CrimeFighter.parseDateStrings(crimes);
+    var crime = crossfilter(crimeArray),
+        all = crime.groupAll(); 
+        date = crime.dimension(function(d) { return d3.time.day(d.date); }),
+        dates = date.group();
+  });
+}
