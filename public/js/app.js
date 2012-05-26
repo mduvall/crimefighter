a624a6711cfca0612bb04f5d48f3546cfa1414f4
Matthew DuVall <mduvall89@gmail.com>
2012-05-25 19:39:39 -0700

    Setup crossfilter and data container/dimensions

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,32 @@
 $(document).ready(function() {
   CrimeFighter.getJSONFromServer();
+  window.setTimeout(CrimeFighter.setupDataContainers, 500);
 });
 
 CrimeFighter = {};
 
 CrimeFighter.getJSONFromServer = function() {
   $.getJSON("/get_crime_json", {}, function(data) {
-    console.log(data);
+    CrimeFighter.rawCrimes = data
   });
+};
+
+CrimeFighter.rawCrimeToArray = function() {
+  if (CrimeFighter.rawCrimes) {
+    var ary = [];
+    for (var crime in CrimeFighter.rawCrimes) {
+      ary.push(CrimeFighter.rawCrimes[crime]);
+    }
+    return ary;
+  }
+  return false;
+};
+
+CrimeFighter.setupDataContainers = function() {
+  var crimeContainer = CrimeFighter.rawCrimeToArray();
+  CrimeFighter.crimeCrossfilter = crossfilter(crimeContainer);
+};
+
+CrimeFighter.dimensionByCrimeType = function() {
+  return CrimeFigher.crimeCrossfilter.dimension(function(crime) { crime.crime; });
 };
